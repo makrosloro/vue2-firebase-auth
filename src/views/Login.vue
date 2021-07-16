f36ad55237d1a9a9494674a88fce95f1d727eec5
makrosloro <lorente.marcos@gmail.com>
2021-07-16 11:19:36 +0200

    New login view and route

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" name="email" id="email" placeholder="Email"><br>
+    <input type="password" name="password" id="password" placeholder="Password"><br>
+    <button>Connect</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{};
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+  .login{
+    margin-top: 40px;
+  }
+</style>
