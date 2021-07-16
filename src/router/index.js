2c4d9364ac8cfec4ff882739b1a402677e70c0e6
makrosloro <lorente.marcos@gmail.com>
2021-07-16 11:39:36 +0200

    New SignUp view component and route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from "@/views/Login";
+import SignUp from "@/views/SignUp";
 
 Vue.use(VueRouter)
 
@@ -15,7 +16,12 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
-  }
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/SignUp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="sign-up">
+    <h3>Let's create a new account</h3>
+    <input type="email" name="email" id="email" placeholder="Email"><br>
+    <input type="password" name="password" id="password" placeholder="Password"><br>
+    <button>Sign Up</button>
+    <span>or <router-link to="/login">go back to login</router-link>.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  data(){
+    return{};
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .sign-up {
+    margin-top: 40px;
+  }
+
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+
+  button {
+    margin-top: 10px;
+    width: 10%;
+    cursor: pointer;
+  }
+
+  span {
+    display: block;
+    margin-top: 20px;
+    font-size: 11px;
+  }
+
+</style>
