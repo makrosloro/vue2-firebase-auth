5dfbdf9817ca77d8509cced0f6c6c89fa313a1a8
makrosloro <lorente.marcos@gmail.com>
2021-07-16 11:39:48 +0200

    Adding signUp link

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,6 +4,7 @@
     <input type="text" name="email" id="email" placeholder="Email"><br>
     <input type="password" name="password" id="password" placeholder="Password"><br>
     <button>Connect</button>
+    <p>You don't have an account? You can <router-link to="/sign-up">create one</router-link></p>
   </div>
 </template>
 
@@ -18,7 +19,30 @@ export default {
 </script>
 
 <style scoped>
-  .login{
+  .login {
     margin-top: 40px;
   }
+
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+
+  p a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
 </style>
