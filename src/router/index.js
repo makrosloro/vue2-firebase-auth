f1942649064cfa33e916633ad707e829c1be9f77
makrosloro <lorente.marcos@gmail.com>
2021-07-16 13:20:02 +0200

    Implements firebase onAuthStatsChanged and make some improvements

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
 import firebase from "firebase";
+import Vue from 'vue'
+
+import App from "../App.vue";
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+//Routes
+import Home from '@/views/Home.vue'
 import Login from "@/views/Login";
 import SignUp from "@/views/SignUp";
 
 Vue.config.productionTip = false;
+
+let app = '';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,14 +25,25 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.auth().onAuthStateChanged(() => {
+  if (!app){
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+})
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: '*',
+    redirect: '/login'
+  },
+  {
+    path: '/',
+    redirect: '/login'
   },
   {
     path: '/login',
@@ -38,12 +55,29 @@ const routes = [
     name: 'SignUp',
     component: SignUp
   },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    meta: {
+      requiresAuth: true,
+    }
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next('login');
+  else if(!requiresAuth && currentUser) next('home');
+  else next();
 })
 
 export default router
